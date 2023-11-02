27899071a782f6f70c6758bf16b9f3104dceefe9
Surabhi2201 <143389194+Surabhi2201@users.noreply.github.com>
2023-11-02 09:43:40 +0530

    Update fibonacci.cpp

--- fibonacci.cpp
+++ fibonacci.cpp
@@ -4,11 +4,11 @@ int main()
 	int a,n,s,b,i;
 	printf("Enter the term:-");
 	scanf("%d",&n);
-	a=0,b=1;
-	for(int i;i<n;i++)
+	a=0,b=1,s;
+	for(int i=0;i<n-2;i++)
 	 {
 	  s=a+b;
-	  i=b;
+	  a=b;
 	  b=s; 
 	 }
 	printf("%d",s);
